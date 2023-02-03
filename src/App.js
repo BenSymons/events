d2d8bbd6d798b999b9cff63ee76040be36c1d09c
BenSymons <bsymonsb@hotmail.co.uk>
2023-02-03 13:44:59 +0000

    create starting point

--- src/App.js
+++ src/App.js
@@ -3,36 +3,14 @@ import FormContainer from './components/FormContainer';
 import Header from './components/Header';
 import PostContainer from './components/PostContainer';
 import postData from "./data/posts.json"
-import { useState, useEffect } from 'react';
-import { throttle } from 'lodash';
 
 function App() {
 
-  const [posts, setPosts] = useState(postData)
-  const [level, setLevel] = useState("shallow")
-  
-  const handleScroll = throttle(() => {
-    console.log("scrolling...")
-    if(window.scrollY > 200) {
-      setLevel("deep")
-    } else if(level==="deep" && window.scrollY <= 200) {
-      setLevel("shallow")
-    }
-  }, 500)
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [level, handleScroll])
-
-
-  // console.log(level, "<-- level")
-
   return (
     <div className="App">
-      { level === "shallow" && <Header/>}
-      <FormContainer setPosts={setPosts}/>
-      <PostContainer posts={posts}/>
+      <Header/>
+      <FormContainer/>
+      <PostContainer posts={postData}/>
     </div>
   );
 }
--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,37 +1,17 @@
-import { useState } from "react"
-
-const FormContainer = ({setPosts}) => {
-    const [newPost, setNewPost] = useState({title: "", body: ""})
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("submitting...")
-        setPosts((prev) => [newPost, ...prev])
-    }
-
-    const handleChange = (e) => {
-        // console.log(e.target.id, "<-- e")
-        const value = e.target.value
-        const key = e.target.id
-        setNewPost((prev) => {
-            return {...prev, [key]: value}
-        })
-    }
+const FormContainer = () => {
 
     return (
         <div className="form-container">
             <h2>Post a new comment</h2>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <label htmlFor="title">title</label>
                 <input 
                     id="title" 
                     placeholder="title"
-                    onChange={handleChange}
                 />
                 <label htmlFor="body">body</label>
                 <textarea 
                     id="body"
-                    onChange={handleChange}
                 />
                 <button type="submit">Post</button>
             </form>
